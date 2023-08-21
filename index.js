ab40af04f1940b5582ebbfb3db80c94bca510dd7
abdullahbinziad <abdullahbinziad@gmail.com>
2023-08-22 02:08:36 +0600

    done

--- index.js
+++ index.js
@@ -1,197 +1,171 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
-var mysql = require("mysql");
 const cors = require("cors");
 const app = express();
+const dotenv = require("dotenv"); // Import dotenv package
+
 
 const port = 4000;
 
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DBNAME,
+//mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kxnyyz3.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
-connection.connect();
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const database = client.db("portfolio");
+    const personalInfoCollection = database.collection("personalInfo");
+    const projectsCollection = database.collection("projects");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+    app.get("/", (req, res) => {
+      res.send("Hello World!");
+    });
 
-app.get("/personalInfo", async (req, res) => {
-  const q = " SELECT * FROM personal_info";
-  await connection.query(q, (err, data) => {
-    if (err) {
-      return res.json(err);
-    }
-    return res.json(data);
-  });
-});
+    app.get("/personalInfo", async (req, res) => {
+      let query = {};
+      const result = await personalInfoCollection.find(query).toArray();
+      res.send(result);
+    });
 
-//get all projects
+    app.get("/projects", async (req, res) => {
+      const result = await projectsCollection.find({}).toArray();
+      res.send(result);
+    });
 
+    app.get("/projects/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new Object(id) };
 
+      const result = await projectsCollection.find(query).toArray();
+      res.send(result);
+    });
 
-app.get("/projects", async (req, res) => {
-  const q = " SELECT * FROM projects";
-  await connection.query(q, (err, data) => {
-    if (err) {
-      return res.json(err);
-    }
-    return res.json(data);
-  });
-});
+    //delete projects
 
-app.get("/projects/:id", async (req, res) => {
-  const id = req.params.id ; 
-  const q = ` SELECT * FROM projects WHERE id = ${id}`;
-  await connection.query(q, (err, data) => {
-    if (err) {
-      return res.json(err);
-    }
-    return res.json(data);
-  });
-});
+    app.delete("/projects/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new Object(id) };
+      const result = await projectsCollection.deleteOne(query);
+      res.send(result);
+    });
 
-//delete projects
+    // update single projects
 
+    app.put("/projects/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectID(id) };
+      const update = {
+        $set: {
+          projectName: req.body.projectName,
+          category: req.body.category,
+          projectsType: req.body.projectsType,
+          features: req.body.features,
+          live_link: req.body.live_link,
+          github_link: req.body.github_link,
+          technology: req.body.technology,
+          projectsImage: req.body.projectsImage,
+        },
+      };
 
+      const options = { upsert: true };
 
-app.delete('/projects/:id', async (req, res) => {
-  const id = req.params.id;
-  const q = 'DELETE FROM projects WHERE id = ?';
+      const result = await projectsCollection.updateOne(
+        filter,
+        update,
+        options
+      );
+      res.send(result);
+    });
 
-  console.log('Delete hit for ID:', id);
+    // post projects
 
-  await connection.query(q, [id], (err, data) => {
-    if (err) {
-      console.error('Error deleting project:', err);
-      return res.status(500).json({ error: 'An error occurred while deleting the project.' });
-    }
-    return res.json(data);
-  });
-});
+    app.post("/addProjects", async (req, res) => {
+      const data = req.body;
+      const result = await projectsCollection.insertOne(data);
+      res.send(result);
+    });
 
-//update projects
+    //update personal info
 
-app.put("/projects/:id", async (req, res) => {
-  const id = req.params.id;
-  const q = "UPDATE projects SET `projectName` = ? , `category` = ?, `projectsType` = ? , `features` = ? , `live_link` = ? , `github_link` = ? , `technology` = ? , `projectsImage` = ? WHERE id = ? ";
+    app.put("/personalInfo/:id", async (req, res) => {
+      const id = 1;
+      const filter = { _id: new ObjectID(id) };
+      const update = {
+        $set: {
+          developerName: req.body.developerName,
+          title: req.body.title,
+          aboutMe: req.body.aboutMe,
+          image: req.body.image,
+        },
+      };
 
-  const values = [
-    req.body.projectName,
-    req.body.category,
-    req.body.projectsType,
-    req.body.features, 
-    req.body.live_link, 
-    req.body.github_link, 
-    req.body.technology, 
-    req.body.projectsImage, 
-    id,
-  ];
+      // Add the 'upsert' option here
+      const options = { upsert: true };
 
-  await connection.query(q, [...values], (err, data) => {
-    if (err) return res.json(err);
-    return res.send("Projects Updated Successfuly");
-  });
-});
+      const result = await personalInfoCollection.updateOne(
+        filter,
+        update,
+        options
+      );
+      res.send(result);
+    });
 
+    //update social links info
 
-//post Projects
+    app.put("/contactInfo/:id", async (req, res) => {
+      const id = 1;
+      const filter = { _id: new ObjectID(id) };
+      const update = {
+        $set: {
+          facebook: req.body.facebook,
+          github: req.body.github,
+          linkdin: req.body.linkdin,
+          youtube: req.body.youtube,
+          mobile: req.body.mobile,
+          location: req.body.location,
+          email: req.body.email,
+          resume_link: req.body.resume_link,
+        },
+      };
 
-app.post("/addProjects", async (req, res) => {
-  const q =
-    "INSERT INTO projects (`projectName`,`category`,`projectsType`, `features`, `live_link` , `github_link`, `technology`,`projectsImage`) VALUES (?)";
+      // Add the 'upsert' option here
+      const options = { upsert: true };
 
-  const values = [
-    
-    req.body.projectName,
-    req.body.category,
-    req.body.projectsType,
-    req.body.features,
-    req.body.live_link,
-    req.body.github_link,
-    req.body.technology,
-    req.body.projectsImage,
-  ];
+      const result = await personalInfoCollection.updateOne(
+        filter,
+        update,
+        options
+      );
+      res.send(result);
+    });
 
-  await connection.query(q, [values], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("Book has been created Successfuly");
-  });
-});
-
-//update the personal info
-
-app.put("/personalInfo/:id", async (req, res) => {
-  const q =
-    "UPDATE personal_info SET `developerName` = ? , `title` = ?, `aboutMe` = ? , `image` = ? WHERE id = ? ";
-  const id = 1;
-  const values = [
-    req.body.developerName,
-    req.body.title,
-    req.body.aboutMe,
-    req.body.image,
-    id,
-  ];
-
-  await connection.query(q, [...values], (err, data) => {
-    if (err) return res.json(err);
-    return res.send("Personal Info Updated Successfuly");
-  });
-});
-
-//update the Socail info
-
-app.put("/contactInfo/:id", async (req, res) => {
-  const q =
-    "UPDATE personal_info SET `facebook` = ? , `github` = ?, `linkdin` = ? , `youtube` = ? , `mobile` = ? , `location` = ? , `email` =? , `resume_link` = ?  WHERE id = ? ";
-  const id = 1;
-  const values = [
-    req.body.facebook,
-    req.body.github,
-    req.body.linkdin,
-    req.body.youtube,
-    req.body.mobile,
-    req.body.location,
-    req.body.email,
-    req.body.resume_link,
-    id,
-  ];
-
-  await connection.query(q, [...values], (err, data) => {
-    if (err) return res.json(err);
-    return res.send("Social Info Updated Successfuly");
-  });
-});
-
-//delete api
-
-app.delete("/books/:id", async (req, res) => {
-  const id = req.params.id;
-  const q = `DELETE FROM books WHERE id=${id}`;
-
-  await connection.query(q, (err, data) => {
-    if (err) return res.json(err);
-    return res.json(`ID ${id} is Deleted`);
-  });
-});
-//update api
-
-app.put("/books/:id", async (req, res) => {
-  const id = req.params.id;
-  const q =
-    "UPDATE books SET `title` = ? , `desc` = ?, `cover` = ? WHERE id = ? ";
-  const values = [req.body.title, req.body.desc, req.body.cover, id];
-
-  await connection.query(q, [...values], (err, data) => {
-    if (err) return res.json(err);
-    return res.json("Data is updated");
-  });
-});
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
