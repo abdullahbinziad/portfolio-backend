a54f3a2e93d81f56cc662d5ab418de03a2fd51b6
abdullahbinziad <abdullahbinziad@gmail.com>
2023-09-22 10:04:35 +0600

    updated code

--- index.js
+++ index.js
@@ -2,20 +2,16 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const dotenv = require("dotenv"); // Import dotenv package
+const dotenv = require("dotenv");
 
-const port = 4000;
+const port = process.env.PORT || 4000; // Use process.env.PORT if available, otherwise use 4000
 
 app.use(express.json());
 app.use(cors());
 dotenv.config();
 
-//mongodb
-
+// MongoDB connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kxnyyz3.mongodb.net/?retryWrites=true&w=majority`;
-
-console.log(uri);
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,133 +20,172 @@ const client = new MongoClient(uri, {
   },
 });
 
-async function run() {
+async function connectToDatabase() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const database = client.db("portfolio");
-    const personalInfoCollection = database.collection("personalInfo");
-    const projectsCollection = database.collection("projects");
+    console.log("Connected to MongoDB");
 
-    app.get("/", (req, res) => {
-      res.send("Hello World!");
-    });
-
-    app.get("/personalInfo", async (req, res) => {
-      const result = await personalInfoCollection.find().toArray();
-      res.send(result);
-    });
-
-    app.get("/projects", async (req, res) => {
-      const result = await projectsCollection
-        .find({})
-        .sort({ createdAt: -1 })
-        .toArray();
-      res.send(result);
-    });
-
-    app.get("/projects/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-
-      const result = await projectsCollection.find(query).toArray();
-      res.send(result);
-    });
-
-    //delete projects
-
-    app.delete("/projects/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: new ObjectId(id) };
-      console.log(query);
-      const result = await projectsCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    // update single projects
-
-    app.put("/projects/:id", async (req, res) => {
-      const id = req.params.id;
-      const filter = { _id: new ObjectId(id) };
-      const update = {
-        $set: {
-          projectName: req.body.projectName,
-          category: req.body.category,
-          projectsType: req.body.projectsType,
-          features: req.body.features,
-          live_link: req.body.live_link,
-          github_link: req.body.github_link,
-          technology: req.body.technology,
-          projectsImage: req.body.projectsImage,
-        },
-      };
-
-      const result = await projectsCollection.updateOne(filter, update);
-      res.send(result);
-    });
-
-    // post projects
-
-    app.post("/addProjects", async (req, res) => {
-      const data = req.body;
-      data.createdAt = new Date();
-      const result = await projectsCollection.insertOne(data);
-      res.send(result);
-    });
-
-    //update personal info
-
-    app.put("/personalInfo/:id", async (req, res) => {
-      const filter = { _id: "1" };
-      const update = {
-        $set: {
-          developerName: req.body.developerName,
-          title: req.body.title,
-          aboutMe: req.body.aboutMe,
-          image: req.body.image,
-        },
-      };
-
-      // Add the 'upsert' option here
-
-      const result = await personalInfoCollection.updateOne(filter, update);
-      res.send(result);
-    });
-
-    //update social links info
-
-    app.put("/contactInfo/:id", async (req, res) => {
-      const filter = { _id: "1" };
-
-      const update = {
-        $set: {
-          facebook: req.body.facebook,
-          github: req.body.github,
-          linkdin: req.body.linkdin,
-          youtube: req.body.youtube,
-          mobile: req.body.mobile,
-          location: req.body.location,
-          email: req.body.email,
-          resume_link: req.body.resume_link,
-        },
-      };
-
-      const result = await personalInfoCollection.updateOne(filter, update);
-      res.send(result);
-    });
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
+    return client.db("portfolio");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
   }
 }
-run().catch(console.dir);
 
+// Define route handlers
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/personalInfo", async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const personalInfoCollection = db.collection("personalInfo");
+    const result = await personalInfoCollection.find().toArray();
+    res.send(result);
+  } catch (error) {
+    console.error("Error fetching personal info:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/projects", async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const projectsCollection = db.collection("projects");
+    const result = await projectsCollection
+      .find({})
+      .sort({ createdAt: -1 })
+      .toArray();
+    res.send(result);
+  } catch (error) {
+    console.error("Error fetching projects:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/projects/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+
+  try {
+    const db = await connectToDatabase();
+    const projectsCollection = db.collection("projects");
+    const result = await projectsCollection.find(query).toArray();
+    res.send(result);
+  } catch (error) {
+    console.error("Error fetching project by ID:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.delete("/projects/:id", async (req, res) => {
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+
+  try {
+    const db = await connectToDatabase();
+    const projectsCollection = db.collection("projects");
+    const result = await projectsCollection.deleteOne(query);
+    res.send(result);
+  } catch (error) {
+    console.error("Error deleting project:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.put("/projects/:id", async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: new ObjectId(id) };
+  const update = {
+    $set: {
+      projectName: req.body.projectName,
+      category: req.body.category,
+      projectsType: req.body.projectsType,
+      features: req.body.features,
+      live_link: req.body.live_link,
+      github_link: req.body.github_link,
+      technology: req.body.technology,
+      projectsImage: req.body.projectsImage,
+    },
+  };
+
+  try {
+    const db = await connectToDatabase();
+    const projectsCollection = db.collection("projects");
+    const result = await projectsCollection.updateOne(filter, update);
+    res.send(result);
+  } catch (error) {
+    console.error("Error updating project:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/addProjects", async (req, res) => {
+  const data = req.body;
+  data.createdAt = new Date();
+
+  try {
+    const db = await connectToDatabase();
+    const projectsCollection = db.collection("projects");
+    const result = await projectsCollection.insertOne(data);
+    res.send(result);
+  } catch (error) {
+    console.error("Error adding project:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.put("/personalInfo/:id", async (req, res) => {
+  const filter = { _id: "1" };
+  const update = {
+    $set: {
+      developerName: req.body.developerName,
+      title: req.body.title,
+      aboutMe: req.body.aboutMe,
+      image: req.body.image,
+    },
+  };
+
+  try {
+    const db = await connectToDatabase();
+    const personalInfoCollection = db.collection("personalInfo");
+    const result = await personalInfoCollection.updateOne(filter, update);
+    res.send(result);
+  } catch (error) {
+    console.error("Error updating personal info:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.put("/contactInfo/:id", async (req, res) => {
+  const filter = { _id: "1" };
+  const update = {
+    $set: {
+      facebook: req.body.facebook,
+      github: req.body.github,
+      linkdin: req.body.linkdin,
+      youtube: req.body.youtube,
+      mobile: req.body.mobile,
+      location: req.body.location,
+      email: req.body.email,
+      resume_link: req.body.resume_link,
+    },
+  };
+
+  try {
+    const db = await connectToDatabase();
+    const personalInfoCollection = db.collection("personalInfo");
+    const result = await personalInfoCollection.updateOne(filter, update);
+    res.send(result);
+  } catch (error) {
+    console.error("Error updating contact info:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// Start the server
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
